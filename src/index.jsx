e94984fe35531e58797a97731292535a2f3f89bb
Aman Jaiswal <87319499+amanjaiswal0777@users.noreply.github.com>
2024-07-03 12:07:39 +0530

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import app from './app.css'
+import './app.css';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
